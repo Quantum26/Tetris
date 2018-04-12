3517399942522eead7a825c8dfe60dea746d4f32
Arjun Pal <anonamause10@gmail.com>
2018-04-12 14:29:04 -0500

    new music player

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -2,6 +2,7 @@ import java.io.*;
 import sun.audio.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.embed.swing.JFXPanel;
 /**
  * Write a description of class MusicPlayer here.
  *
@@ -19,41 +20,49 @@ public class MusicPlayer
         muted = false;
         length = l;
     }
+
     public void setMusic(String s, double l){
         music = s;
         length = l;
     }
+
     public void setMuted(boolean m){
         muted = m;
     }
+
     public String getMusic(){
         return music;
     }
+
     public boolean getMuted(){
         return muted;
     }
+
     public double getLength(){
         return length;
     }
+
     public void music(){
         if(!muted){
-            AudioPlayer MGP = AudioPlayer.player;
-            AudioStream BGM;
-            AudioData MD;
-            ContinuousAudioDataStream loop = null;
+            /**
             try{
-                InputStream in = new FileInputStream(music); 
-                as = new AudioStream(in);
-                AudioPlayer.player.start(as);
+            InputStream in = new FileInputStream(music); 
+            as = new AudioStream(in);
+            AudioPlayer.player.start(as);
             }catch(IOException e){
-                System.out.println("Baka");
-            }
-            
+            System.out.println("Baka");
+            }*/
+            String bip = "Fingerdash.mp3";
+            JFXPanel fxPanel = new JFXPanel();
+            Media hit = new Media(new File(bip).toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(hit);
+            mediaPlayer.play();
+            int x = 0;
         }
     }
 
     public void stopMusic(){
-       
+
         try
         {
             //don't try and do things with a null object!
